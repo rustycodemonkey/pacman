3f14bd99d4a95c0e8e481de5c7538da1e0eb40fe
Phillip Kim <philkim78@gmail.com>
2019-11-21 18:21:31 +1100

    First cut of the docstrings and comments.

--- test_files/py_test/pacman.ut.py
+++ test_files/py_test/pacman.ut.py
@@ -14,32 +14,32 @@ class AllTests(unittest.TestCase):
     def test_generic(self):
         self.assertEqual(pacman("generic.txt"), (6, 1, 27))
 
-    # def test_no_walls(self):
-    #     self.assertEqual(pacman("no_walls.txt"), (2, 1, 11))
+    def test_no_walls(self):
+        self.assertEqual(pacman("no_walls.txt"), (2, 1, 11))
 
-    # def test_no_walls_loop(self):
-    #     self.assertEqual(pacman("no_walls_loop.txt"), (1, 1, 11))
+    def test_no_walls_loop(self):
+        self.assertEqual(pacman("no_walls_loop.txt"), (1, 1, 11))
 
-    # def test_no_walls_loops(self):
-    #     self.assertEqual(pacman("no_walls_loops.txt"), (4, 1, 10))
+    def test_no_walls_loops(self):
+        self.assertEqual(pacman("no_walls_loops.txt"), (4, 1, 10))
 
-    # def test_trapped_middle(self):
-    #     self.assertEqual(pacman("trapped_middle.txt"), (3, 3, 0))
+    def test_trapped_middle(self):
+        self.assertEqual(pacman("trapped_middle.txt"), (3, 3, 0))
 
-    # def test_trapped_corner(self):
-    #     self.assertEqual(pacman("trapped_corner.txt"), (4, 4, 0))
+    def test_trapped_corner(self):
+        self.assertEqual(pacman("trapped_corner.txt"), (4, 4, 0))
 
-    # def test_edge(self):
-    #     self.assertEqual(pacman("edge.txt"), (-1, -1, 0))
+    def test_edge(self):
+        self.assertEqual(pacman("edge.txt"), (-1, -1, 0))
 
-    # def test_edge_perimeter(self):
-    #     self.assertEqual(pacman("edge_perimeter.txt"), (-1, -1, 0))
+    def test_edge_perimeter(self):
+        self.assertEqual(pacman("edge_perimeter.txt"), (-1, -1, 0))
 
-    # def test_edge_wall(self):
-    #     self.assertEqual(pacman("edge_wall.txt"), (-1, -1, 0))
+    def test_edge_wall(self):
+        self.assertEqual(pacman("edge_wall.txt"), (-1, -1, 0))
 
-    # def test_runtime(self):
-    #     self.assertEqual(pacman("runtime.txt"), (2142, 147, 148))
+    def test_runtime(self):
+        self.assertEqual(pacman("runtime.txt"), (2142, 147, 148))
 
 
 if __name__ == '__main__':
